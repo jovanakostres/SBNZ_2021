38418160d60798f11dc2cb6db665d7e4916bece9
Jovana Kostres <jovanako15@gmail.com>
2021-04-25 23:06:29 +0200

    forward chaining drools

--- PreporukaOdevneKombinacije/src/main/java/com/ftn/PreporukaOdevneKombinacije/model/KomadOdece.java
+++ PreporukaOdevneKombinacije/src/main/java/com/ftn/PreporukaOdevneKombinacije/model/KomadOdece.java
@@ -3,11 +3,13 @@ package com.ftn.PreporukaOdevneKombinacije.model;
 import com.ftn.PreporukaOdevneKombinacije.model.enums.*;
 
 public abstract class KomadOdece {
+    private Long id;
     private Boja boja;
     private BojaIntenzitet bojaIntenzitet;
     private Materijal materijal;
     private Vreme vreme;
     private int prioritet;
+    private double koeficijentOdabira;
 
     public KomadOdece(Boja boja, BojaIntenzitet bojaIntenzitet, Materijal materijal, Vreme vreme, int prioritet) {
         this.boja = boja;
