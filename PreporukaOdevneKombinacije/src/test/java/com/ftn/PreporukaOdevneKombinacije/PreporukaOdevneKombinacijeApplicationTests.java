f8444809cff0d4ef68dbe3e5569d3b0cefd66f69
Jovana Kostres <jovanako15@gmail.com>
2021-04-25 15:29:34 +0200

    Initial project

---
+++ PreporukaOdevneKombinacije/src/test/java/com/ftn/PreporukaOdevneKombinacije/PreporukaOdevneKombinacijeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.PreporukaOdevneKombinacije;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreporukaOdevneKombinacijeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
