53d915bafbd81fe2b7a1dccd61d349d901e4de49
saramik <52412719+saramik@users.noreply.github.com>
2021-05-16 20:54:35 +0200

    api za vreme

--- PreporukaOdevneKombinacije/src/main/java/com/ftn/PreporukaOdevneKombinacije/service/KomadOdeceService.java
+++ PreporukaOdevneKombinacije/src/main/java/com/ftn/PreporukaOdevneKombinacije/service/KomadOdeceService.java
@@ -4,16 +4,46 @@ import com.ftn.PreporukaOdevneKombinacije.dto.UnosDTO;
 import com.ftn.PreporukaOdevneKombinacije.model.KomadOdece;
 import com.ftn.PreporukaOdevneKombinacije.model.User;
 import com.ftn.PreporukaOdevneKombinacije.model.drlModel.PreporuceniKomadi;
+import com.ftn.PreporukaOdevneKombinacije.model.enums.Vreme;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.nio.charset.Charset;
 import java.util.List;
 
 @Service
 public class KomadOdeceService {
 
-    public PreporuceniKomadi getPreporuceniGornjiDeo(UnosDTO unosDTO, User user) {
+    public PreporuceniKomadi getPreporuceniGornjiDeo(UnosDTO unosDTO, User user){
         PreporuceniKomadi preporuceniKomadi1 = new PreporuceniKomadi();
+
+        //vreme start
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + unosDTO.getMesto() + "&appid=8a45d678f270270943ef5016b28f55e3&units=metric";
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(((HttpURLConnection) (new URL(url)).openConnection()).getInputStream(), Charset.forName("UTF-8")));
+            String resenje = reader.readLine();
+            reader.close();
+            JsonObject jsonObject = new JsonParser().parse(resenje).getAsJsonObject();
+            float temp = Float.parseFloat(jsonObject.getAsJsonObject("main").get("temp").toString());
+            JsonArray jArray =  jsonObject.getAsJsonArray("weather");//getAsJsonObject("symbol");
+            String weather = jArray.get(0).getAsJsonObject().get("main").toString();
+            unosDTO.setTemperatura(temp);
+            if(weather.equals("Thunderstorm") || weather.equals("Drizzle") || weather.equals("Rain") || weather.equals("Snow"))
+                unosDTO.setVreme(Vreme.VLAZNO);
+            else
+                unosDTO.setVreme(Vreme.SUVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //vreme end
 
         return preporuceniKomadi1;
     }
