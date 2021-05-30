c4d76fe893d0006c63cc68439d2a9196bdc0ac62
Jovana Kostres <jovanako15@gmail.com>
2021-05-30 22:38:58 +0200

    test suite

---
+++ PreporukaOdevneKombinacije/src/test/java/com/ftn/PreporukaOdevneKombinacije/TestAll.java
@@ -0,0 +1,11 @@
+package com.ftn.PreporukaOdevneKombinacije;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CepIzvestajTests.class, DonjiDeoPersonalizovanoTests.class, GornjiDeoPersonalizovanoTests.class, JaknaPersonalizovanoTest.class, ObucaPersonalizovanoTest.class,
+        QueryPersTests.class
+})
+public class TestAll {
+}
